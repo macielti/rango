6f32a05bb8264592b320665eb33c70e62f551360
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-06 00:04:47 -0300

    last attempt

--- project.clj
+++ project.clj
@@ -45,6 +45,7 @@
                                      "--no-server"
                                      "--allow-incomplete-classpath"
                                      "--initialize-at-build-time"
+                                     "--initialize-at-run-time=rango.components"
                                      "--enable-url-protocols=http,https"
                                      "--trace-object-instantiation=java.lang.Thread"
                                      "-Dio.pedestal.log.defaultMetricsRecorder=nil"
