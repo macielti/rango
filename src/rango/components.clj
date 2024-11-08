63d81aab31d1dd8f7935b8aef7da9063201d4d00
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-08 06:21:24 -0300

    cleaner code

--- src/rango/components.clj
+++ src/rango/components.clj
@@ -1,9 +1,9 @@
 (ns rango.components
-  (:require [common-clj.integrant-components.config]
-            [common-clj.integrant-components.http-client]
-            [common-clj.integrant-components.prometheus]
-            [common-clj.integrant-components.routes]
-            [common-clj.integrant-components.service]
+  (:require [common-clj.integrant-components.config :as component.config]
+            [common-clj.integrant-components.http-client :as component.http-client]
+            [common-clj.integrant-components.prometheus :as component.prometheus]
+            [common-clj.integrant-components.routes :as component.routes]
+            [common-clj.integrant-components.service :as component.service]
             [integrant.core :as ig]
             [new-relic-component.core :as component.new-relic]
             [porteiro-component.admin-component :as porteiro.admin]
@@ -16,20 +16,20 @@
 (taoensso.timbre.tools.logging/use-timbre)
 
 (def config
-  {:common-clj.integrant-components.config/config           {:path "resources/config.edn"
-                                                             :env  :prod}
-   :common-clj.integrant-components.prometheus/prometheus   {:metrics []}
-   :common-clj.integrant-components.http-client/http-client {:components {:config     (ig/ref :common-clj.integrant-components.config/config)
-                                                                          :prometheus (ig/ref :common-clj.integrant-components.prometheus/prometheus)}}
-   ::component.new-relic/new-relic                          {:components {:config      (ig/ref :common-clj.integrant-components.config/config)
-                                                                          :http-client (ig/ref :common-clj.integrant-components.http-client/http-client)}}
-   ::porteiro.admin/admin                                   {:components {:config     (ig/ref :common-clj.integrant-components.config/config)
-                                                                          :postgresql (ig/ref ::component.postgresql/postgresql)}}
-   ::component.postgresql/postgresql                        {:components {:config (ig/ref :common-clj.integrant-components.config/config)}}
-   :common-clj.integrant-components.routes/routes           {:routes (concat diplomat.http-server/routes porteiro.diplomat.http-server/routes)}
-   :common-clj.integrant-components.service/service         {:components {:config     (ig/ref :common-clj.integrant-components.config/config)
-                                                                          :routes     (ig/ref :common-clj.integrant-components.routes/routes)
-                                                                          :postgresql (ig/ref ::component.postgresql/postgresql)}}})
+  {::component.config/config           {:path "resources/config.edn"
+                                        :env  :prod}
+   ::component.prometheus/prometheus   {:metrics []}
+   ::component.http-client/http-client {:components {:config     (ig/ref ::component.config/config)
+                                                     :prometheus (ig/ref ::component.prometheus/prometheus)}}
+   ::component.new-relic/new-relic     {:components {:config      (ig/ref ::component.config/config)
+                                                     :http-client (ig/ref ::component.http-client/http-client)}}
+   ::porteiro.admin/admin              {:components {:config     (ig/ref ::component.config/config)
+                                                     :postgresql (ig/ref ::component.postgresql/postgresql)}}
+   ::component.postgresql/postgresql   {:components {:config (ig/ref ::component.config/config)}}
+   ::component.routes/routes           {:routes (concat diplomat.http-server/routes porteiro.diplomat.http-server/routes)}
+   ::component.service/service         {:components {:config     (ig/ref ::component.config/config)
+                                                     :routes     (ig/ref ::component.routes/routes)
+                                                     :postgresql (ig/ref ::component.postgresql/postgresql)}}})
 
 (defn start-system! []
   (ig/init config))
